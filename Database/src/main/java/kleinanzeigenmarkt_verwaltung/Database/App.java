ca0c195882538401c5654557c9c0ee3dcaa0f9f8
Sven <Sven@10.226.6.13>
2014-12-17 12:35:59 +0100

    Create project

---
+++ Database/src/main/java/kleinanzeigenmarkt_verwaltung/Database/App.java
@@ -0,0 +1,13 @@
+package kleinanzeigenmarkt_verwaltung.Database;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ GUI/src/main/java/kleinanzeigenmarkt_verwaltung/GUI/App.java
@@ -0,0 +1,13 @@
+package kleinanzeigenmarkt_verwaltung.GUI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
